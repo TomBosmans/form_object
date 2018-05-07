2e529e5be26a1d781b9278889bae14baf953d7f1
TomBosmans <t.bosse@hotmail.com>
2018-05-07 16:20:07 +0200

    moved require in engine.rb to the bottom
    although this worked in the dummy app, this breaks
    in an actual app when on top.

--- lib/form_object/engine.rb
+++ lib/form_object/engine.rb
@@ -1,8 +1,8 @@
-require 'form_object/base'
-require 'form_object/field'
-
 module FormObject
   class Engine < ::Rails::Engine
     isolate_namespace FormObject
   end
 end
+
+require 'form_object/base'
+require 'form_object/field'
